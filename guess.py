88974816b591786c5c7a84e83d1199c8aa3b610c
一壶兔 <gongjinmin@outlook.com>
2018-09-10 14:48:48 +0800

    判断年龄

--- guess.py
+++ guess.py
@@ -1,7 +1,7 @@
 age_of_princal = 56
 guess_age = int( input(">>:") )
 
-'''if guess_age == age_of_princal then  ##伪代码，不能执行
+'''if guess_age == age_of_princal then  ##伪代码，不能执行。
 	print("yes")
 else
 	print("no")
