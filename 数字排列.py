1c42b39174e0fe2c101d17ba46dc6a462af04b43
yihutu <gongjinmin@outlook.com>
2018-09-14 14:22:38 +0800

    数字排列.py

---
+++ 数字排列.py
@@ -0,0 +1,11 @@
+#conding:utf-8
+num1 = 0     
+while num1<=5:
+    print(num1,end="_")
+    num2 = 0
+    while num2<=7:
+        print(num2,end="-")
+        num2 +=1
+        
+    num1 +=1
+    print()
